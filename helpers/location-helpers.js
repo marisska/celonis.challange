eb93609325b1ba50e2f5b1ecbc3f5dbf0ce2efa8
Marina Tyurina <>
2018-12-15 14:51:08 +0100

    Add other health checks

--- helpers/location-helpers.js
+++ helpers/location-helpers.js
@@ -10,12 +10,14 @@ module.exports = {
   },
   getElementByClass(className) {
     var ele = element(by.css(`.${className}`));
-    browser.wait(EC.visibilityOf(ele), timeout, "Custom Error Message");
+    browser.wait(EC.visibilityOf(ele), timeout, 
+            `No element with class ${className} is found.`);
     return ele;
   },
   getElementByClassAndText(className,text) {
     var ele = element(by.cssContainingText(`.${className}`, text));
-    browser.wait(EC.visibilityOf(ele), timeout, "Custom Error Message");
+    browser.wait(EC.visibilityOf(ele), timeout, 
+            `No element with class ${className} and text ${text} is found.`);
     return ele;
   },
   getButton(buttonText) {
--- specs/health-check.js
+++ specs/health-check.js
@@ -6,16 +6,66 @@ const account = require('../account.js');
 describe('Health check', function() {
     navigator.navigateTo('/');
 
-    interactor.enterTextByName('email', account.loginData.email);
-    interactor.enterTextByName('password', account.loginData.pass);
-    interactor.clickButton('Log in');
-       
-    interactor.clickByClass('project-item-name', 'Example Projects');
-    interactor.clickByClass('action-bar__item', 'Open');
-    interactor.clickByClass('project-item-name','1. Purchase to Pay');
-    interactor.clickByClass('action-bar__item', 'Open');
-     
+        
+    it('should show user name', function() {
+        interactor.enterTextByName('email', account.loginData.email);
+        interactor.enterTextByName('password', account.loginData.pass);
+        interactor.clickButton('Log in');
+
+        reader.shouldReadTextByClass('account-holder', account.loginData.name);
+     }); 
+    
     it('should show Purchase to pay process', function() {
+        interactor.clickByClass('project-item-name', 'Example Projects');
+        interactor.clickByClass('action-bar__item', 'Open');
+        interactor.clickByClass('project-item-name','1. Purchase to Pay');
+        interactor.clickByClass('action-bar__item', 'Open');
+     
         reader.shouldReadTextByClass('ce-component-title','Purchase to pay process');
+    });
+
+    it('should show Accounts payable process', function() {
+        browser.navigate().back();
+
+        interactor.clickByClass('project-item-name','2. Accounts Payable');
+        interactor.clickByClass('action-bar__item', 'Open');
+     
+        reader.shouldReadTextByClass('ce-component-title','Accounts payable process');
+    });
+
+    it('should show Order to cash process', function() {
+        browser.navigate().back();
+
+        interactor.clickByClass('project-item-name','3. Order to Cash');
+        interactor.clickByClass('action-bar__item', 'Open');
+     
+        reader.shouldReadTextByClass('ce-component-title','Order to cash process');
+    });
+
+    it('should show IT Service Management process', function() {
+        browser.navigate().back();
+
+        interactor.clickByClass('project-item-name','4. IT Service Management');
+        interactor.clickByClass('action-bar__item', 'Open');
+     
+        reader.shouldReadTextByClass('ce-component-title','IT Service Management process');
+    });
+
+    it('should show Logistics process', function() {
+        browser.navigate().back();
+
+        interactor.clickByClass('project-item-name','5. Logistics');
+        interactor.clickByClass('action-bar__item', 'Open');
+     
+        reader.shouldReadTextByClass('ce-component-title','Logistics process');
+    });
+
+    it('should show Human resources payable process', function() {
+        browser.navigate().back();
+
+        interactor.clickByClass('project-item-name','6. Human Resources');
+        interactor.clickByClass('action-bar__item', 'Open');
+     
+        reader.shouldReadTextByClass('ce-component-title','Human resources process');
     });
 });
